b7ee5264d57770566cb8a041ac75d6fa382d0121
John Lorica <jroderick.0717@gmail.com>
2026-05-09 23:44:28 +0800

    Finished 10 minutes of a Tkinter tutorial

--- Tkinter.py
+++ Tkinter.py
@@ -5,4 +5,14 @@ root = tk.Tk()
 root.geometry("500x500")
 root.title("My First GUI")
 
+label = tk.Label(root, text="Hello World!", font=('Arial, 18'))
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, height=3, font=('Arial', 16))
+textbox.pack(padx=10, pady=10)
+
+button = tk.Button(root, text="Click Me!", font=('Arial', 18))
+button.pack(padx=10, pady=10)
+
+
 root.mainloop()
