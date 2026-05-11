64f9ddce2141bf0d0f2328d9e3cd6f6eb4d66ace
John Lorica <jroderick.0717@gmail.com>
2026-05-11 21:43:28 +0800

    Doing the tutorial properly

--- Tkinter.py
+++ Tkinter.py
@@ -17,20 +17,20 @@ buttonframe.columnconfigure(1, weight=1)
 buttonframe.columnconfigure(2, weight=1)
 
 btn1 = tk.Button(buttonframe, text="1", font=('Arial', 18))
-btn1.grid(row=0, column=1, sticky=tk.W+tk.E)
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
 
 btn2 = tk.Button(buttonframe, text="2", font=('Arial', 18))
-btn2.grid(row=0, column=2, sticky=tk.W+tk.E)
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
 
 btn3 = tk.Button(buttonframe, text="3", font=('Arial', 18))
-btn3.grid(row=0, column=3, sticky=tk.W+tk.E)
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
 
 btn4 = tk.Button(buttonframe, text="4", font=('Arial', 18))
-btn4.grid(row=0, column=4, sticky=tk.W+tk.E)
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
 
 btn5 = tk.Button(buttonframe, text="5", font=('Arial', 18))
-btn5.grid(row=0, column=5, sticky=tk.W+tk.E)
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
 
 btn6 = tk.Button(buttonframe, text="6", font=('Arial', 18))
-btn6.grid(row=0, column=6, sticky=tk.W+tk.E)
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
 root.mainloop()
